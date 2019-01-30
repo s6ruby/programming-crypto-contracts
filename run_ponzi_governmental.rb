e01b6d8a14b6b43ed2eb2580cc4322fea6e88f1b
Gerald Bauer <gerald.bauer@gmail.com>
2019-01-30 21:28:33 +0100

    Add files via upload

---
+++ run_ponzi_governmental.rb
@@ -0,0 +1,48 @@
+# encoding: utf-8
+
+require_relative './lib/universum'
+require_relative './ponzi_governmental'
+
+
+###
+# test contract
+
+## setup test accounts with starter balance
+Account[ '0x1111' ].balance = 1_000_000
+Account[ '0xaaaa' ].balance = 1_000_000
+Account[ '0xbbbb' ].balance = 1_000_000
+Account[ '0xcccc' ].balance = 1_000_000
+Account[ '0xdddd' ].balance = 1_000_000
+Account[ '0xeeee' ].balance = 1_000_000
+
+## (pp) pretty print all known accounts with balance
+pp Uni.accounts
+
+## genesis (founder)
+Uni.msg = { sender: '0x1111', value: 1_000_000 }
+gov = Governmental.new
+pp gov
+
+Uni.send_transaction( from: '0xaaaa', to: gov, value: 1_000_000 )
+pp gov
+Uni.send_transaction( from: '0xbbbb', to: gov, value: 1_000_000, data: [:lend_government_money, '0xaaaa'] )
+pp gov
+Uni.send_transaction( from: '0xcccc', to: gov, value: 1_000_000 )
+pp gov
+Uni.send_transaction( from: '0xdddd', to: gov, value: 1_000_000 )
+pp gov
+Uni.send_transaction( from: '0xeeee', to: gov, value: 1_000_000 )
+pp gov
+
+## (pp) pretty print all known accounts with balance
+pp Uni.accounts
+
+## mine - move block time ahead more than 12h (use 13h)
+Uni.block = { number: 1, timestamp: Time.now.to_i + 60*60*13 }
+
+## economy will collapse and new (ponzi) round starts
+Uni.send_transaction( from: '0xeeee', to: gov, value: 1_000_000 )
+pp gov
+
+## (pp) pretty print all known accounts with balance
+pp Uni.accounts
